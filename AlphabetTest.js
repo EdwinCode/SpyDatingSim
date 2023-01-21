9a6fb29b0642ba1c12918d85d6b3d145636abbed
Chloe D <owlgirlpnw@gmail.com>
2023-01-20 17:36:30 -0800

    Initial alphabet class stub

---
+++ AlphabetTest.js
@@ -0,0 +1,18 @@
+class AlphabetTest {
+    constructor(game) {
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites/sprite_alphabet_8.png"), 8, 8, 48, 72, 5, 0.5);
+
+        this.x = 200;
+        this.y = 700;
+        this.speed = 100;
+    };
+
+    update() {
+        this.x = 200;
+    };
+
+    draw(ctx) {
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    };
+};
