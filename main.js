b97c2453f66c78019c0a56f4538478d6f1261d01
Chloe D <owlgirlpnw@gmail.com>
2023-01-20 17:36:53 -0800

    Updated index and main for stubs

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./Sprites/sprite_girl_purple.png");
+ASSET_MANAGER.queueDownload("./Sprites/sprite_boy_brown.png");
+ASSET_MANAGER.queueDownload("./Sprites/sprite_alphabet_8.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new PurpleGirl(gameEngine));
+	gameEngine.addEntity(new SpyCharacter(gameEngine));
+	gameEngine.addEntity(new BillionaireCharacter(gameEngine));
+	gameEngine.addEntity(new AlphabetTest(gameEngine));
 
 	gameEngine.init(ctx);
 
