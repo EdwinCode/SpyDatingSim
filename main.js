30b9bd01996434f37d9e625adbc6aeff932717eb
Chloe D <owlgirlpnw@gmail.com>
2023-01-25 01:04:12 -0800

    Removed unnecessary comments

--- main.js
+++ main.js
@@ -2,18 +2,8 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-//backgrounds
-
 //entities
 ASSET_MANAGER.queueDownload("./Sprites/sprite_girl_purple.png");
-
-//ASSET_MANAGER.queueDownload("./Sprites/sprite_boy_brown.png");
-//ASSET_MANAGER.queueDownload("./Sprites/sprite_alphabet_x8.png");
-//ASSET_MANAGER.queueDownload("./Sprites/date_candidate_1.png");
-//ASSET_MANAGER.queueDownload("./Sprites/date_candidate_2.png");
-//ASSET_MANAGER.queueDownload("./Sprites/guard.png");
-
-//cutscenes
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
