077f5281fd86d0d9d41fe6a7651bc2ff6d7c1533
Maria <mariababko4292@gmail.com>
2023-02-09 22:40:11 -0800

    Added timer and darkness for the quick time event

---
+++ entities/ingame_timer.js
@@ -0,0 +1,51 @@
+class IngameTimer {
+    constructor(game) {
+        this.game = game;
+
+        this.time = 200;
+
+    };
+
+    update() {
+        if (this.timer === undefined) {
+            this.timer = 0;
+        } else {
+            this.timer += this.game.clockTick;
+        }
+
+        if (this.timer > 0.4) {
+            this.time -= 1;
+            this.timer = undefined;
+        }
+    };
+
+    draw(ctx) {
+        this.setBlackStroke(ctx);
+        ctx.lineWidth = 4;
+        ctx.textAlign = "center";
+
+        ctx.fillStyle = "#d9d9d9";
+
+        // circle
+        ctx.beginPath();
+        ctx.arc(100, 580, 60, 0, 2 * Math.PI, false);
+        ctx.fill();
+        ctx.stroke();
+
+        this.setBlackStroke(ctx);
+
+        // Timer
+        ctx.font = "Bold 30px Courier";
+        ctx.fillText("Timer", 100, 565);
+        ctx.font = "Bold 25px Courier";
+        ctx.fillStyle = "Red";
+        ctx.fillText(this.time, 100, 605);
+
+    };
+
+    setBlackStroke(ctx) {
+        ctx.strokeStyle = "Black";
+        ctx.fillStyle = "Black";
+    };
+
+}
--- gameengine.js
+++ gameengine.js
@@ -162,6 +162,10 @@ class GameEngine {
         this.entities.push(entity);
     };
 
+    addEntityToTop(entity) {
+      this.entities.unshift(entity) ;
+    };
+
     draw() {
         // Clear the whole canvas with transparent color (rgba(0, 0, 0, 0))
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
