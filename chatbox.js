d74747edb3bff8f0eafe09f6eca2d700ad686c13
Chloe D <owlgirlpnw@gmail.com>
2023-02-12 16:48:51 -0800

    Initial chat box class

---
+++ chatbox.js
@@ -0,0 +1,21 @@
+class ChatBox {
+    constructor(game, text) {
+        this.game = game;
+        this.text = text;
+
+        this.chatboxX = 0;
+        this.chatboxY;
+        this.chatboxW;
+        this.chatboxH;
+
+
+    };
+
+    update() {
+
+    };
+
+    draw(ctx) {
+
+    };
+}
