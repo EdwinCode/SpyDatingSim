a0ef516163eebea0e0939403f2f68f6fdd1cdcb1
Chloe D <owlgirlpnw@gmail.com>
2023-02-14 10:02:11 -0800

    stubbed hint chat

--- chatbox.js
+++ chatbox.js
@@ -56,10 +56,40 @@ class CasefileChatbox {
 };
 
 class HintChat {
-    constructor(game) {
+    constructor(game, level) {
         this.game = game;
+        this.level = level;
+
+        // determine which hint to give based on character interaction
         this.numItems = 0;
 
+        this.hints = [];
+        this.loadHints();
+        //this.hideHints = true;
+    };
+
+    loadHints() {
+        // jagged array
+        for (let i = 0; i < 2; i++) { // one level, two parts
+            this.hints.push([]);
+        }
+
+        for (let j = 0; j < 2; j++) { // two hints for lvl 1 pt 1
+            this.hints[1].push([]);
+        }
+
+        for (let j = 0; j < 3; j++) { // three hints for lvl 1 pt 2
+            this.hints[1].push([]);
+        }
+
+        // level one, part one hints
+        this.hints[0][0] = "Talk to Mr.Billionaire.";
+        this.hints[0][1] = "Talk to Stephanie and Richie.";
+
+        // level one, part two hints
+        this.hints[1][0] = "Look in the trashcan.";
+        this.hints[1][1] = "Look at the table surfaces.";
+        this.hints[1][2] = "Look in the cabinets near the wet table.";
     };
 
     update() {
