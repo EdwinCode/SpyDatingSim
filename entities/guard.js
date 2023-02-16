c92e7dca983a6bb4600dd64981b37f8c33994f0e
Edwin <solisbrunoedwin@gmail.com>
2023-02-15 19:59:32 -0800

    Refactored Guard to use BLOCKWIDTH

--- entities/guard.js
+++ entities/guard.js
@@ -4,18 +4,18 @@ class Guard{
         this.game = game;
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/entities/guard.png");
 
-        this.guardW = 130;
-        this.guardH = 200;
+        this.guardW = 14 * PARAMS.BLOCKWIDTH;
+        this.guardH = 25 * PARAMS.BLOCKWIDTH;
 
-        this.x = -200;
-        this.y = -20;
+        this.x = -100 * PARAMS.BLOCKWIDTH;
+        this.y = -80 * PARAMS.BLOCKWIDTH;
 
         this.velocity = 70;
 
         this.direction = 0;
 
 
-        this.wanderBB = new BoundingBox(this.x, this.y, 400,this.guardH / 2);
+        this.wanderBB = new BoundingBox(this.x, this.y, this.guardW * 10,this.guardH * 2);
 
         this.updateBB();
         this.updateSightBB();
@@ -26,16 +26,16 @@ class Guard{
 
     updateBB() {
         this.lastBB = this.BB;
-        this.BB = new BoundingBox(this.x, this.y + 40, this.guardW / 2 - 12, this.guardH / 2 - 40);
+        this.BB = new BoundingBox(this.x, this.y, this.guardW, this.guardH);
     }
 
     updateSightBB() {
         // this.lastSightBB = this.sightBB;
         if (this.direction === 0) {
-            this.sightBB = new BoundingBox(this.x, this.y + 40, this.wanderBB.width - (this.x - this.wanderBB.x), this.guardH / 2 - 40);
+            this.sightBB = new BoundingBox(this.x, this.y, this.wanderBB.width - (this.x - this.wanderBB.x), this.guardH);
 
         } else {
-            this.sightBB = new BoundingBox(this.wanderBB.x, this.y + 40, this.x - (this.wanderBB.x - this.guardW/2.45), this.guardH / 2 - 40);
+            this.sightBB = new BoundingBox(this.wanderBB.x, this.y, this.x - (this.wanderBB.x - this.guardW / 2), this.guardH / 2);
         }
 
     }
@@ -77,7 +77,7 @@ class Guard{
     };
 
     draw(ctx) {
-        this.animations[this.direction].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y - this.game.camera.y, PARAMS.SCALE / 6);
+        this.animations[this.direction].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y - this.game.camera.y, PARAMS.SCALE);
 
         PARAMS.DEBUG = document.getElementById("debug").checked;
         if (PARAMS.DEBUG) {
