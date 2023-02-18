5936516c9b9c9f0dc0ba6dba0d23a69db189580e
Maria <mariababko4292@gmail.com>
2023-02-17 19:53:10 -0800

    Added NPC Collision and removed deleted billionaire file from index.html

--- entities/npc.js
+++ entities/npc.js
@@ -13,6 +13,7 @@ class NPC {
         this.updateInteractionBB();
 
         this.animations = [];
+        this.furnitureCollided = false;
 
     }
 
@@ -32,7 +33,7 @@ class NPC {
 
         // choose a random direction after a # of rounds
         if (this.directionDuration === 0) {
-            console.log("zero");  // debug
+            //console.log("zero");  // debug
 
             this.direction = this.chooseRandDirection();
             // random duration to keep animator the same for
@@ -44,40 +45,65 @@ class NPC {
             }
         }
 
+        var that = this;
+        this.game.entities.forEach(function (entity) {
+            //if the entity has a bounding box and we collided with it
+            if (entity.BB && that.BB.collide(entity.BB)) {
+                if (entity instanceof  Furniture|| entity instanceof NPC) {
+                    if(entity.BB.right <= (that.lastBB.left+20)){ // from right
+                        that.direction = that.chooseRandDirection();
+                    }
+                    else if(entity.BB.left >= (that.lastBB.right-20)){ // from left
+                        that.direction = that.chooseRandDirection();
+                    }
+                    else if(entity.BB.bottom <= (that.lastBB.top+20)){ //from below
+                        that.direction = that.chooseRandDirection();
+                    }
+                    else if(entity.BB.top >= (that.lastBB.bottom-20)){ // from above
+                        that.direction = that.chooseRandDirection();
+                    }
+                }
+            }
+        });
+
         // else still duration
         // no collision
-        if (this.collides(this.direction) === false) {
+        if (that.collides(that.direction) === false) {
             // down
-            if (this.direction === 0) {
-                this.y += this.velocity * this.game.clockTick;
+            if (that.direction === 0) {
+                that.y += that.velocity * that.game.clockTick;
             }
 
             // up
-            else if (this.direction === 1) {
-                this.y -= this.velocity * this.game.clockTick;
+            else if (that.direction === 1) {
+                that.y -= that.velocity * that.game.clockTick;
             }
 
             // left
-            else if (this.direction === 2) {
-                this.x -= this.velocity * this.game.clockTick;
+            else if (that.direction === 2) {
+                that.x -= that.velocity * that.game.clockTick;
             }
 
             // right
             else {
-                this.x += this.velocity * this.game.clockTick;
+                that.x += that.velocity * that.game.clockTick;
             }
 
-            this.directionDuration -= 1;
+            that.directionDuration -= 1;
         }
 
         // collision
         else {
             // choose new direction but keep duration same
-            while (this.collides(this.direction)) {
-                this.direction = this.chooseRandDirection();
+
+            while (that.collides(that.direction)) {
+                that.direction = that.chooseRandDirection();
             }
         }
+
     };
+
+
 
     // returns 0, 1, 2, 3
     chooseRandDirection() {
