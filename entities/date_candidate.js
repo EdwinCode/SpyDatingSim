18b1ebcb357bcc746e70ef31535e80265bc609fa
Maria <mariababko4292@gmail.com>
2023-02-17 18:55:18 -0800

    Fixed Richie Sprite and made parent NPC class

--- entities/date_candidate.js
+++ entities/date_candidate.js
@@ -1,16 +1,7 @@
-class Stephanie {
-    constructor(game) {
-        this.game = game;
-
-        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/entities/stephanie.png");
-
-        // pulled out repeated number to a variable
-        this.billionaireW = 16 * PARAMS.BLOCKWIDTH;
-        this.billionaireH = 29 * PARAMS.BLOCKWIDTH;
-
-        // character placement
-        this.x = 100 * PARAMS.BLOCKWIDTH;
-        this.y = 100 * PARAMS.BLOCKWIDTH;
+class NPC {
+    constructor(game, spritesheet, x, y, dWidth, dHeight) {
+        Object.assign(this, {game, x, y, dWidth, dHeight});
+        this.spritesheet = ASSET_MANAGER.getAsset(spritesheet);
 
         this.velocity = 70;
         this.direction = 0;
@@ -22,37 +13,18 @@ class Stephanie {
         this.updateInteractionBB();
 
         this.animations = [];
-        this.loadAnimations();
-    };
 
-    setStephaniePlacement(x, y) {
-        this.x = x;
-        this.y = y;
     }
-
-    setWanderBBPlacement(x, y) {
-        this.wanderX = x;
-        this.wanderY = y;
-    }
-
-    loadAnimations() {
-        // walking animation
-        // 0 = down, 1 = up, 2 = left, 3 = right
-        this.animations[0] = new Animator(this.spritesheet, 8, 8, 144, 208, 4, 0.3);
-        this.animations[1] = new Animator(this.spritesheet, 8, 215, 150, 208, 4, 0.3);
-        this.animations[2] = new Animator(this.spritesheet, 8, 420, 144, 208, 4, 0.3);
-        this.animations[3] = new Animator(this.spritesheet, 8, 628, 144, 208, 4, 0.3);
-
-    };
 
     updateBB() {
         this.lastBB = this.BB;
-        this.BB = new BoundingBox(this.x, this.y, this.billionaireW, this.billionaireH);
+        this.BB = new BoundingBox(this.x, this.y, this.dWidth, this.dHeight);
     };
 
     updateInteractionBB() {
-        this.interactBB = new BoundingBox(this.x - this.billionaireW / 2, this.y - this.billionaireH / 2, this.billionaireW * 2, this.billionaireH * 2);
+        this.interactBB = new BoundingBox(this.x - this.dWidth / 2, this.y - this.dHeight / 2, this.dWidth * 2, this.dHeight * 2);
     };
+
 
     update() {
         this.updateBB();
@@ -156,44 +128,17 @@ class Stephanie {
 
         }
     };
-};
 
-class Richie {
-    constructor(game) {
-        this.game = game;
+}
 
-        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/entities/richie.png");
 
-        // pulled out repeated number to a variable
-        this.billionaireW = 16 * PARAMS.BLOCKWIDTH;
-        this.billionaireH = 29 * PARAMS.BLOCKWIDTH;
+class Billionaire extends NPC{
+    constructor(game, x, y) {
+        super(game, "./sprites/entities/billionaire.png", x, y, 16 * PARAMS.BLOCKWIDTH, 29 * PARAMS.BLOCKWIDTH);
 
-        // character placement
-        this.x = 100 * PARAMS.BLOCKWIDTH;
-        this.y = 100 * PARAMS.BLOCKWIDTH;
-
-        this.velocity = 70;
-        this.direction = 0;
-        this.directionDuration = 500;
-
-        this.wanderBB = new BoundingBox(this.x, this.y, 200 * PARAMS.BLOCKWIDTH, 104 * PARAMS.BLOCKWIDTH);
-
-        this.updateBB();
-        this.updateInteractionBB();
-
-        this.animations = [];
         this.loadAnimations();
     };
 
-    setRichiePlacement(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    setWanderBBPlacement(x, y) {
-        this.wanderX = x;
-        this.wanderY = y;
-    }
 
     loadAnimations() {
         // walking animation
@@ -205,115 +150,43 @@ class Richie {
 
     };
 
-    updateBB() {
-        this.lastBB = this.BB;
-        this.BB = new BoundingBox(this.x, this.y, this.billionaireW, this.billionaireH);
+}
+
+class Stephanie extends NPC{
+    constructor(game, x, y) {
+        super(game, "./sprites/entities/stephanie.png", x, y, 16 * PARAMS.BLOCKWIDTH, 29 * PARAMS.BLOCKWIDTH);
+
+        this.loadAnimations();
     };
 
-    updateInteractionBB() {
-        this.interactBB = new BoundingBox(this.x - this.billionaireW / 2, this.y - this.billionaireH / 2, this.billionaireW * 2, this.billionaireH * 2);
+
+    loadAnimations() {
+        // walking animation
+        // 0 = down, 1 = up, 2 = left, 3 = right
+        this.animations[0] = new Animator(this.spritesheet, 8, 8, 144, 208, 4, 0.3);
+        this.animations[1] = new Animator(this.spritesheet, 8, 215, 150, 208, 4, 0.3);
+        this.animations[2] = new Animator(this.spritesheet, 8, 420, 144, 208, 4, 0.3);
+        this.animations[3] = new Animator(this.spritesheet, 8, 628, 144, 208, 4, 0.3);
+
+    };
+}
+
+class Richie extends NPC{
+    constructor(game, x, y) {
+        super(game, "./sprites/entities/richie.png", x, y, 16 * PARAMS.BLOCKWIDTH, 29 * PARAMS.BLOCKWIDTH);
+
+        this.loadAnimations();
     };
 
-    update() {
-        this.updateBB();
-        this.updateInteractionBB();
 
-        // choose a random direction after a # of rounds
-        if (this.directionDuration === 0) {
-            console.log("zero");  // debug
+    loadAnimations() {
+        // walking animation
+        // 0 = down, 1 = up, 2 = left, 3 = right
+        this.animations[0] = new Animator(this.spritesheet, 8, 8, 128, 208, 4, 0.3);
+        this.animations[1] = new Animator(this.spritesheet, 8, 212, 128, 208, 4, 0.3);
+        this.animations[2] = new Animator(this.spritesheet, 8, 420, 110, 208, 4, 0.3);
+        this.animations[3] = new Animator(this.spritesheet, 8, 628, 110, 208, 4, 0.3);
 
-            this.direction = this.chooseRandDirection();
-            // random duration to keep animator the same for
-            this.directionDuration = 500;
-
-            // keep choosing new direction if there is collision detected
-            while (this.collides(this.direction)) {
-                this.direction = this.chooseRandDirection();
-            }
-        }
-
-        // else still duration
-        // no collision
-        if (this.collides(this.direction) === false) {
-            // down
-            if (this.direction === 0) {
-                this.y += this.velocity * this.game.clockTick;
-            }
-
-            // up
-            else if (this.direction === 1) {
-                this.y -= this.velocity * this.game.clockTick;
-            }
-
-            // left
-            else if (this.direction === 2) {
-                this.x -= this.velocity * this.game.clockTick;
-            }
-
-            // right
-            else {
-                this.x += this.velocity * this.game.clockTick;
-            }
-
-            this.directionDuration -= 1;
-        }
-
-        // collision
-        else {
-            // choose new direction but keep duration same
-            while (this.collides(this.direction)) {
-                this.direction = this.chooseRandDirection();
-            }
-        }
     };
 
-    // returns 0, 1, 2, 3
-    chooseRandDirection() {
-        return Math.floor(Math.random() * 4);
-    };
-
-    collides(direction) {
-        // down
-        if (direction === 0) {
-            // return true if going down and there is a down collision
-            return this.lastBB.y + this.lastBB.height >= this.wanderBB.y + this.wanderBB.height;
-        }
-
-        // up
-        else if (direction === 1) {
-            // return true if going up and there is an up collision
-            return this.lastBB.y <= this.wanderBB.y;
-        }
-
-        // left
-        else if (direction === 2) {
-            // return true if going left and there is a left collision
-            return this.lastBB.x <= this.wanderBB.x;
-        }
-
-        // right
-        else {
-            // return true if going right and there is a right collision
-            return this.lastBB.x + this.lastBB.width >= this.wanderBB.x + this.wanderBB.width;
-        }
-    };
-
-    draw(ctx) {
-        this.animations[this.direction].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y - this.game.camera.y, PARAMS.SCALE);
-
-        // debug
-        PARAMS.DEBUG = document.getElementById("debug").checked;
-        if (PARAMS.DEBUG) {
-            ctx.lineWidth = 4;
-            ctx.strokeStyle = 'red';
-            ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y - this.game.camera.y, this.BB.width, this.BB.height);
-
-            ctx.strokeStyle = 'green';
-            ctx.strokeRect(this.interactBB.x - this.game.camera.x, this.interactBB.y - this.game.camera.y, this.interactBB.width, this.interactBB.height);
-
-            ctx.strokeStyle = 'blue';
-            ctx.strokeRect(this.wanderBB.x - this.game.camera.x, this.wanderBB.y - this.game.camera.y, this.wanderBB.width, this.wanderBB.height);
-
-        }
-    };
-};
+}
