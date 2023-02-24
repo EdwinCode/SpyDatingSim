39f783225802dda7e6de513c7eafb6295ff18876
Edwin <solisbrunoedwin@gmail.com>
2023-02-24 03:06:09 -0800

    Made the rooms much smaller and fixed most furniture placements

--- levels/levels.js
+++ levels/levels.js
@@ -3,103 +3,71 @@ let levelOne1 = {
 
     // THE UNITS OF MEASUREMENT FOR THE BELOW CODE ARE IN BLOCKWIDTHS!
 
-    //background
-    sideWallLefts: [ // count makes them grow downwards
-        // Room 1 left walls
-        {x: 0, y: 0, count: 23},
-        {x: 0, y: -368, count: 23}
+    sideWallLefts: [ // Count makes them grow downwards
+        {x: 0, y: 0, count: 17},
+        {x: 0, y: -240, count: 15}
     ],
 
     plainWalls: [
-        // Room 1 top walls
         // Start at x = 4.5 because the Room 1 left wall starts at 0 and a left wall takes up 4.5 BLOCKWIDTHS so 0 + 4.5 = 4.5.
-        {x: 4.5, y: 0, count: 4}, // A plain wall piece has a width of 31 BLOCKWIDTHS. Thus, this ends at x = 4.5 + 10 * 31 = 314.5
-        {x: 190.5, y: 0, count: 4},
+        {x: 4.5, y: 0, count: 3}, // A plain wall piece has a width of 31 BLOCKWIDTHS. Count makes them grow rightward
+        {x: 159.5, y: 0, count: 3},
 
-        {x: 319, y: 0, count: 4},
-        {x: 505, y: 0, count: 4},
-        {x: 4.5, y: -368, count: 10},
-        {x: 319, y: -368, count: 10},
+        {x: 257, y: 0, count: 3},
+        {x: 412, y: 0, count: 3},
 
-        // Room 1 bottom walls
-        /* Start at y = 336 because of the following math to line up the bottom edge:
-        The Room 1 left walls start at 0, have a height of 16, count of 23.
-        A plain wall piece has a height of 32 BLOCKWIDTHS.
-        We have 23 walls on the left side.
-        0 + (16 * 23) - 32 = 336
-         */
-        {x: 4.5, y: 336, count: 10},
-        {x: 319, y: 336, count: 10}
+        {x: 4.5, y: -240, count: 3},
+        {x: 159.5, y:-240, count: 3},
+
+        {x: 257, y: -240, count: 3},
+        {x: 412, y: -240, count: 3},
+
+        {x: 4.5, y: 240, count: 8},
+        {x: 257, y: 240, count: 8}
     ],
 
-    sideWallRights: [
-        // Room 1 right walls
-        // The Room 1 plain walls end at x = 314.5, so we start there. Line it up to the left wall with y = 0.
-        {x: 314.5, y: 0, count: 10},
-        // We have 10 walls (height 16), then 2 big rugs (height 24) before we start these walls so:
-        // y = 0 + (16 * 10) + (24 * 2) = 208
-        {x: 314.5, y: 208, count: 10},
+    sideWallRights: [ // Count makes them grow downwards
+        {x: 252.5, y: 0, count: 7},
+        {x: 252.5, y: 160, count: 7},
 
-        {x: 314.5, y: -368, count: 10},
-        {x: 314.5, y: -160, count: 10},
+        {x: 252.5, y: -240, count: 7},
+        {x: 252.5, y: -80, count: 5},
 
-        {x: 629, y: 0, count: 23},
-        {x: 629, y: -368, count: 23}
+        {x: 505, y: 0, count: 17},
+        {x: 505, y: -240, count: 15}
     ],
 
     bigRugs: [
-        // We want half the rug in and out of the rooms.
-        /*
-        Room 1 right rugs:
-        For top rug, y = 0 + (16 * 10) = 160
-        For bottom rug y = top rug + 24 = 160 + 24 = 184
-         */
-        {x: 296.25, y: 160},
-        {x: 296.25, y: 184},
+        {x: 234.25, y: 112},
+        {x: 234.25, y: 136},
 
-        {x: 296.25, y: -208},
-        {x: 296.25, y: -184}
+        {x: 234.25, y: -128},
+        {x: 234.25, y: -104}
     ],
 
-    //furniture
     bigTables: [
-        // Room 1 big tables, place wherever they look nice
-        {x: 68, y: 64},
-        {x: 242, y: 64}
+        {x: 55.5, y: 64},
+        {x: 173.5, y: 64}
     ],
 
     bigCouches: [
-        /*
-        Room 1 couch, place at the center bottom of room.
-        x = (Width of room / 2) - (couch width / 2) = [(Room end of right wall - beginning of left wall) / 2] - [couch width / 2]
-        x = ((164 - 0) / 2) - (32/2) = 66
-        y = [(Room top of bottom wall) - (couch height)] = 128 - 13 = 115
-         */
-        {x: 66, y: 115},
-        {x: 240, y: 115}
+        {x: 53.5, y: 115},
+        {x: 171.5, y: 115}
     ],
 
     chairRights: [
-        // Room 1 chairs, place directly next to the table (x) and center it relative to table (y).
-        // x = (table start - chair width) = (68 - 14) = 54
-        // y = (table top placement + [table height / 2] - [chair height / 2]) = (64 + (32 / 2) - (16 / 2)) = 72
-        {x: 54, y: 72},
-        // x = distance between tables + chair from first table
-        // x = (242 - 68) + 54 = 174 + 54 = 228
-        {x: 228, y: 72}
+        {x: 41.5, y: 72},
+        {x: 159.5, y: 72}
     ],
 
     chairLefts: [
-        // Room 1 chair, place directly next to the table (x) and center it relative to table which centered in the room (y).
-        // x = (table start + table width) = (68 + 28) = 96
-        // y is same as Room 1 chair left.
-        {x: 96, y: 72},
+        {x: 83.5, y: 72},
         // x = 96 + 174
-        {x: 270, y: 72}
+        {x: 201.5, y: 72}
     ],
 
     beds: [
-        {x: 500, y: -150}
+        {x: 445.5, y: -126}
     ],
 
     //NPC PORTRAIT NUMBERS
